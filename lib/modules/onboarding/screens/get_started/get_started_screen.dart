42aa6be6c8f8e8a7e725b3ffcb944b9af9319586
niyiayooluwa <aytholu@gmail.com>
2026-08-20 16:56:43 +0100

    feat(onboarding): add Get Started, Onboarding, and Splash screens with navigation

---
+++ lib/modules/onboarding/screens/get_started/get_started_screen.dart
@@ -0,0 +1,211 @@
+import 'dart:ui';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:slipwise/modules/auth/screens/google_auth/google_auth_notifier.dart';
+import 'package:slipwise/modules/auth/screens/shared/user_notifier.dart';
+
+class GetStartedScreen extends HookConsumerWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/image/skylake.jpg', fit: BoxFit.cover),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: _buildSocialLogin(context, ref),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSocialLogin(BuildContext context, WidgetRef ref) {
+    final theme = ShadTheme.of(context);
+    final isGoogleLoading = ref.watch(googleAuthProvider).isLoading;
+
+    ref.listen(googleAuthProvider, (previous, next) {
+      if (next is AsyncError) {
+        ShadToaster.of(context).show(
+          ShadToast.destructive(
+            title: const Text('Google Sign-In Failed'),
+            description: Text(next.error.toString()),
+          ),
+        );
+      } else if (next is AsyncData &&
+          !next.isLoading &&
+          previous?.isLoading == true) {
+        if (ref.read(userProvider).value != null) {
+          context.go('/home');
+        }
+      }
+    });
+
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(24),
+        topRight: Radius.circular(24),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 16.0, sigmaY: 16.0),
+
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white.withValues(alpha: 0.1),
+            border: Border(
+              top: BorderSide(
+                color: Colors.white.withValues(alpha: 0.2),
+                width: 1.5,
+              ),
+            ),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
+            ),
+          ),
+
+          child: SafeArea(
+            top: false,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Get started',
+                    style: theme.textTheme.h3,
+                    textAlign: TextAlign.center,
+                  ),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    'Get started',
+                    style: theme.textTheme.p,
+                    textAlign: TextAlign.center,
+                  ),
+
+                  SizedBox(height: 20),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ShadButton(
+                      size: ShadButtonSize.lg,
+                      onPressed: isGoogleLoading
+                          ? null
+                          : () => context.push('/login'),
+                      leading: const Icon(LucideIcons.mail),
+                      child: const Text("Sign in with Email"),
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ShadButton.outline(
+                      decoration: ShadDecoration(
+                        border: ShadBorder.all(
+                          color: theme.colorScheme.primary,
+                        ),
+                      ),
+                      foregroundColor: Colors.white,
+                      size: ShadButtonSize.lg,
+                      onPressed: isGoogleLoading
+                          ? null
+                          : () {
+                              ref.read(googleAuthProvider.notifier).signIn();
+                            },
+                      leading: isGoogleLoading
+                          ? null
+                          : SvgPicture.asset(
+                              "assets/drawables/google.svg",
+                              height: 18,
+                              width: 18,
+                            ),
+                      child: isGoogleLoading
+                          ? const SizedBox(
+                              child: SpinKitThreeBounce(
+                                size: 16,
+                                color: Colors.white,
+                              ),
+                            )
+                          : const Text("Continue with Google"),
+                    ),
+                  ),
+
+                  /*SizedBox(height: 16),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ShadButton.outline(
+                      size: ShadButtonSize.lg,
+                      onPressed: () {},
+                      //backgroundColor: Colors.white,
+                      leading: SvgPicture.asset(
+                        "assets/drawables/apple.svg",
+                        height: 18,
+                        width: 18,
+                      ),
+                      child: Text("Sign in with Apple"),
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                child: ShadButton.outline(
+                  size: ShadButtonSize.lg,
+                  onPressed: () {},
+                  //backgroundColor: Colors.white,
+                  leading: SvgPicture.asset("assets/drawables/fb.svg"),
+                  child: Text("Sign in with Facebook"),
+                ),
+              ),*/
+                  SizedBox(height: 24),
+
+                  Text.rich(
+                    TextSpan(
+                      // 1. The base text (inherits default color and style)
+                      text: "New to SlipWise? ",
+                      //style: theme.textTheme.small.copyWith(color: Colors.white60),
+                      children: [
+                        // 2. First clickable part (different color)
+                        TextSpan(
+                          text: 'Create account',
+                          style: TextStyle(
+                            color: theme
+                                .colorScheme
+                                .primary, // Matches your Shadcn primary color
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              context.push('/register');
+                            },
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/onboarding/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:slipwise/core/storage/settings_service.dart';
+
+class OnboardingScreen extends HookConsumerWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ShadTheme.of(context);
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(color: theme.colorScheme.background),
+
+        Image.asset('assets/image/onboard.jpg', fit: BoxFit.cover),
+
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.transparent, // 1. Top edge (fully clear)
+                Colors.black.withValues(
+                  alpha: 0.4,
+                ), // 2. Middle (slight darkening)
+                Colors.black.withValues(
+                  alpha: 0.9,
+                ), // 3. The sudden drop to pure black
+                Colors.black, // 4. Bottom edge (stays solid)
+              ],
+              stops: const [
+                0.0, // Start clear at the very top
+                0.45, // Slowly transition to a slight tint by the 50% mark
+                0.65, // BAM! Suddenly transition to pure black in just this 15% window
+                1.0, // Keep it pure black from 65% all the way to the bottom (100%)
+              ],
+            ),
+          ),
+        ),
+
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+            child: Column(
+              crossAxisAlignment: .start,
+              children: [
+                Spacer(),
+                Text(
+                  'Track & Analyze matches. Master your bets.',
+                  style: theme.textTheme.h1.copyWith(color: Colors.white),
+                ),
+                SizedBox(height: 16),
+
+                Text(
+                  'Import your tickets instantly to get real-time status updates and deep betting insights all in one place.',
+                  style: theme.textTheme.p.copyWith(color: Colors.white70),
+                ),
+                SizedBox(height: 48),
+
+                // Login
+                SizedBox(
+                  width: double.infinity,
+                  child: ShadButton(
+                    size: ShadButtonSize.lg,
+                    trailing: Icon(LucideIcons.arrowRight),
+                    onPressed: () async {
+                      final settings = await ref.read(
+                        settingsServiceProvider.future,
+                      );
+                      await settings.setHasCompletedOnboarding();
+                      if (context.mounted) context.go('/get-started');
+                    },
+                    child: Text('Get Started'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
